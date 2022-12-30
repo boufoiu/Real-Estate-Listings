1b5402ebb43cc9eebd190b24ee622d4fe97f2eca
KaaliidBen <kk_benzid@esi.dz>
2022-12-30 19:47:46 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect} from "react";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 import "./styles/App.css"
 import "./styles/welcomePage.css"
 
@@ -12,8 +13,8 @@ function App() {
    <div className='main-div'>
     <Navbar></Navbar> 
     <div className="welcome-text">
-      <h1>Le <span className="org-text">1er</span> site de <br/> l’immobilier en <br/> Algérie </h1>
-      <h3>Accordez plus de visibilité à vos annonces immobilières 
+      <h1 className="landing-text1">Le <span className="org-text">1er</span> site de <br/> l’immobilier en <br/> Algérie </h1>
+      <h3 className="landing-text2">Accordez plus de visibilité à vos annonces immobilières 
          <br/> et aidez les gens à trouver les meilleurs endroits</h3>
       <button className="start-button">Commencer</button>
       <ul>
@@ -23,6 +24,21 @@ function App() {
       </ul>
     </div>
     <image className="main-image"></image>
+    <SearchBar></SearchBar>
+    <div className="welcome-text2">
+      <h1 className="landing-text3">Darek, Meilleur endroit <br/> en ligne pour les <span className="blue-text">meilleures</span>
+        <br/> <span className="org-text">offres</span> immobilières </h1>
+      <h3 className="landing-text4">Vous aussi, Vous êtes un(e) Agence immobilière!
+         <br/> Donner de la visibilité a votre Agence immobilière
+         <br/> et de vos annonces immobilieres, inscrivez-vous
+         <br/> gratuitement sur Darek et publiez vos
+         <br/> vos annonces immobilière</h3>
+      <div className="buttons-container">
+        <button className="share-button">Partager une offre</button>
+        <button className="navigate-button">Consulter une offre</button>
+      </div>
+    </div>
+    <image className="main-image2"></image>
    </div>
   );
 }
