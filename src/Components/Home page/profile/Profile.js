4f8ec8f6bd38c6d228e315414bde52abc5c33e1b
KaaliidBen <kk_benzid@esi.dz>
2023-01-06 18:00:03 +0100

    home/profile

--- src/Components/Home page/profile/Profile.js
+++ src/Components/Home page/profile/Profile.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import OffersVisualizer from '../Offers/OffersVisualizer'
+import profileImage from '../../../images/profile.jpg'
+import '../../../styles/home page/profile/profile.css'
 
 export default function Profile() {
+  const [myOffers, setMyOffers] = useState([
+    {
+      title: 'Beautiful big house',
+      price: '200,501',
+      description: 'nice house with a great location and a sea view, 5 bedrooms and two bathrooms!'
+    },
+    {
+      title: 'Beautiful big house',
+      price: '200,501',
+      description: 'nice house with a great location and a sea view, 5 bedrooms and two bathrooms!'
+    },
+    {
+      title: 'Beautiful big house',
+      price: '200,501',
+      description: 'nice house with a great location and a sea view, 5 bedrooms and two bathrooms!'
+    },
+    {
+      title: 'Beautiful big house',
+      price: '200,501',
+      description: 'nice house with a great location and a sea view, 5 bedrooms and two bathrooms!'
+    }
+  ])
   return (
-    <div>Profile</div>
+    <div className='profile-ctn'>
+      <div className='profile-info'>
+        <div className='title'>
+          Profile
+        <i class="fa-solid fa-pen-to-square"></i>
+        </div>
+        <img src = {profileImage}></img>
+        <div>Amina Razi</div>
+        <div>amina34@gmail.com</div>
+        <div>+213(0)556984213</div>
+      </div>
+      <OffersVisualizer offers = {myOffers} place = {'profile'} title = {'Voici les annonces que vous avez partage'}/>
+    </div>
   )
 }
