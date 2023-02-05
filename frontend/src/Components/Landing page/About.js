5e1e293fc6160feeb5d54d3ee4d35f14d9646712
YousraFarhani <jy_farhani@esi.dz>
2023-02-05 23:38:29 +0100

    new

--- frontend/src/Components/Landing page/About.js
+++ frontend/src/Components/Landing page/About.js
@@ -7,10 +7,10 @@ export default function About() {
       <div className="about">
         <div>
           <div className="about-title">A propos</div>
-          <div className="about-text">Lorem Ipsum is simply dummy text<br/>
-           of the printing and typesetting industry.<br/>
-            Lorem Ipsum has been the industry's standard<br/>
-             dummy text ever since the 1500s</div>
+          <div className="about-text"> Darek est une entreprise certifiée<br/>
+           avec plus de 10 ans d'experience dans <br/>
+            le domaine des biens immobilieres<br/>
+          </div>
         </div>  
         <div>
           <div className="about-title">Nos service</div>
