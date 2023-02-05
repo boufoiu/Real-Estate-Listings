69aa5ab74019f36d29f9c95be9f63421e64b31c1
YousraFarhani <jy_farhani@esi.dz>
2023-02-05 23:30:49 +0100

    here

--- frontend/src/Components/Landing page/Subscription.js
+++ frontend/src/Components/Landing page/Subscription.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import "../../styles/landing-page/subscription.css"
 import subImage from "../../images/sub-img.jpg"
+import Typewriter from "typewriter-effect";
 
 export default function Subscription() {
   return (
     <div className="subscription">
       <img src={subImage} className="sub-img" ></img>
       <div className="sub-text">
-        <h1 className="sub-text1">abonnez vous pour recevoir les dernieres
-           actualites de darek</h1>
+        <h1 className="sub-text1">
+        <Typewriter
+            options={{
+              strings: ["Abonnez vous pour recevoir les dernieres actualites de darek"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+          </h1>
 
-        <h3 className="sub-text2">rien ne vaudra jamais la satisfaction d'un client, heureux de
+        <h3 className="sub-text2">Rien ne vaudra jamais la satisfaction d'un client, heureux de
           <br/> l'expérience qu'il vient de vivre</h3>
 
         <div className="sub-email">
