60173f4d168183f5569fb283f4ee698e2fa10b93
YousraFarhani <jy_farhani@esi.dz>
2023-02-06 00:21:03 +0100

    animations

--- frontend/src/Components/Landing page/Share.js
+++ frontend/src/Components/Landing page/Share.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import landingImage2 from "../../images/landing-image-2.jpg"
 import "../../styles/landing-page/share.css"
-
+import Typewriter from "typewriter-effect";
 export default function Share() {
   return (
     <div className="content2">
       <img src={landingImage2} className="main-image2"></img>
       <div className="welcome-text2">
-        <h1 className="landing-text3">Darek, Meilleur endroit en ligne pour les <span className="blue-text">meilleures </span>
+        <h1 className="landing-text3">
+        <Typewriter
+            options={{
+              strings: ["Darek, Meilleur endroit en ligne pour les"],
+              autoStart: true,
+              loop: true,
+            }}
+          /> <span className="blue-text">meilleures </span>
         <span className="org-text">offres</span> immobilières </h1>
         <h3 className="landing-text4">Vous aussi, Vous êtes un(e) Agence immobilière!
            Donner de la visibilité a votre Agence immobilière
--- frontend/src/Components/Landing page/Welcome.js
+++ frontend/src/Components/Landing page/Welcome.js
@@ -3,7 +3,8 @@ import landingImage1 from "../../images/landing-image-1.jpg"
 import "../../styles/landing-page/start.css"
 import CreateAccount from '../Forms/CreateAccount'
 import Navbar from './Navbar'
-
+import Typewriter from "typewriter-effect";
+import { Link } from 'react-router-dom'
 export default function Welcome() {
   return (
     <div className="landing-page1">
@@ -11,9 +12,15 @@ export default function Welcome() {
       <div className="content">
         <div className="welcome-text">
           <div className="landing-text1">Le <span className="org-text">1er</span> site de <br/> l’immobilier en<br/> Algérie </div>
-          <div className="landing-text2">Accordez plus de visibilité à vos annonces immobilières 
-            <br/> et aidez les gens à trouver les meilleurs endroits</div>
-          <button className="start-button">Commencer</button>
+          <div className="landing-text2">
+          <Typewriter
+            options={{
+              strings: ["Accordez plus de visibilité à vos annonces immobilières et aidez les gens à trouver les meilleurs endroits"],
+              autoStart: true,
+              loop: true,
+            }}
+          /></div>
+          <Link to = '/connect'><button className='start-button'>Commencer</button></Link>
           <ul className="stats">
             <li><span className="big-text"><span className="org-text">+</span> 2K</span> <br/> <span className="middle-text">Annonces</span></li>
             <li><span className="big-text"><span className="blue-text">+</span> 5K <br/></span> <span className="middle-text">Clients fideles</span></li>
