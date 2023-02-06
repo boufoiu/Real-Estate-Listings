19431a85ec2dd7bed6ad0cd39c8e323fa62afa13
Rayane Djouah <kr_djouah@esi.dz>
2023-02-07 00:21:09 +0100

    google maps

--- frontend/src/Components/Home page/Offers/OfferDetails.js
+++ frontend/src/Components/Home page/Offers/OfferDetails.js
@@ -74,6 +74,27 @@ export default function OfferDetails() {
           ></img>
         ))}
       </div>
+      <div class="mapouter">
+        <div class="gmap_canvas">
+          <iframe
+            width="600"
+            height="500"
+            id="gmap_canvas"
+            src={
+              "https://maps.google.com/maps?q=+" +
+              Commune +
+              "%20" +
+              Wilaya +
+              "Alger&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            }
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+          <br />
+        </div>
+      </div>
     </div>
   );
 }
