349cde4b0d34f855e4c819f776f8c18bbd509279
YousraFarhani <jy_farhani@esi.dz>
2023-02-06 22:06:00 +0100

    changes subscription

--- frontend/src/Components/Landing page/Subscription.js
+++ frontend/src/Components/Landing page/Subscription.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "../../styles/landing-page/subscription.css";
 import subImage from "../../images/sub-img.jpg";
+import Typewriter from "typewriter-effect";
 
 export default function Subscription() {
   return (
     <div className="subscription">
-      <img src={subImage} className="sub-img"></img>
+      <img src={subImage} className="sub-img" ></img>
       <div className="sub-text">
         <h1 className="sub-text1">
-          abonnez vous pour recevoir les dernieres actualites de darek
-        </h1>
+        <Typewriter
+            options={{
+              strings: ["Abonnez vous pour recevoir les dernieres actualites de darek"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+          </h1>
 
-        <h3 className="sub-text2">
-          Rien ne vaudra jamais la satisfaction d'un client, heureux de
-          <br /> l'expérience qu'il vient de vivre
-        </h3>
+        <h3 className="sub-text2">Rien ne vaudra jamais la satisfaction d'un client, heureux de
+          <br/> l'expérience qu'il vient de vivre</h3>
 
         <div className="sub-email">
           <input className="email-input" type={"email"}></input>
@@ -22,5 +27,7 @@ export default function Subscription() {
         </div>
       </div>
     </div>
-  );
+
+
+  )
 }
